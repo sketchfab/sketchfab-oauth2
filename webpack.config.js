4b34e51698aff4a0157eef95d9bee25440ea7f27
Maurice Svay <maurice@sketchfab.com>
2016-12-16 11:44:44 +0100

    Adds version to dist bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+var version = require( './package.json' ).version;
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'sketchfab-oauth2.js',
+        filename: 'sketchfab-oauth2.' + version + '.js',
         path: './dist',
         library: 'SketchfabOAuth2',
-        libraryTarget:'umd'
+        libraryTarget: 'umd'
     }
 }
